9576875a9798025afeb547a947602041dc452824
noursaadallah <nour.saadallah@gmail.com>
2017-08-24 13:45:33 +0200

    update scripts

--- scripts/setupHF.sh
+++ scripts/setupHF.sh
@@ -17,9 +17,9 @@
 	if [ -d "$GOPATH/src/github.com/hyperledger/fabric-sdk-go" ]
 	then
 		cd $GOPATH/src/github.com/hyperledger/fabric-sdk-go && git checkout 85fa3101eb4694d464003c3a900672d632f17833
-		echo "installing fabric-sdk-go dependencies"
-		echo ""
-		cd $GOPATH/src/github.com/hyperledger/fabric-sdk-go && make
+		# echo "installing fabric-sdk-go dependencies"
+		# echo ""
+		# cd $GOPATH/src/github.com/hyperledger/fabric-sdk-go && make
 	fi
 
 	
@@ -45,20 +45,18 @@
 		echo ""
 		echo "fabric & fabric-ca & fabric-sdk-go setup done"
 		echo ""
+		echo "installing fabric-sdk-go client packages"
+		echo ""
+		sudo apt install libltdl-dev
+		go get github.com/hyperledger/fabric-sdk-go/pkg/fabric-client
+		go get github.com/hyperledger/fabric-sdk-go/pkg/fabric-ca-client
+
+		echo ""
+		echo "setup external libs"
+		echo ""
+		go get -u github.com/spf13/viper
+		go get -u github.com/kardianos/govendor
+		# make govendor executable just in case $PATH doesn't include $GOPATH/bin
+		cp $GOPATH/bin/govendor /usr/local/bin/ 
+		cd $GOPATH/src/github.com/noursaadallah/kidner && govendor init && govendor add +external
 	fi
-
-	echo ""
-	echo "installing fabric & fabric-ca packages"
-	echo ""
-	sudo apt install libltdl-dev
-	go get github.com/hyperledger/fabric-sdk-go/pkg/fabric-client
-	go get github.com/hyperledger/fabric-sdk-go/pkg/fabric-ca-client
-
-	echo ""
-    echo "setup external libs"
-	echo ""
-	go get -u github.com/spf13/viper
-	go get -u github.com/kardianos/govendor
-	# make govendor executable just in case $PATH doesn't include $GOPATH/bin
-	cp $GOPATH/bin/govendor /usr/local/bin/ 
-	cd $GOPATH/src/github.com/noursaadallah/kidner && govendor init && govendor add +external
